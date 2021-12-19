af90b5d3aa7730df39a68b6308a6f42fdafdf365
Johnycrown <oladejijohn60@gmail.com>
2021-12-19 23:18:49 +0100

    customer class attribute added

--- src/main/java/com/example/customermanagementsystem/data/model/Customer.java
+++ src/main/java/com/example/customermanagementsystem/data/model/Customer.java
@@ -1,5 +1,14 @@
 package com.example.customermanagementsystem.data.model;
 
+import org.springframework.data.annotation.Id;
+
 public class Customer {
+    @Id
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+
+
 
 }
